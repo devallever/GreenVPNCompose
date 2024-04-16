21b024fde31414ca870c69476a35b1b4d32d231d
devallever <devallever@163.com>
2024-04-16 08:40:50 +0800

    optimize: loading activity finish right now

--- app/src/main/java/com/allever/compose/green/vpn/LoadingActivity.kt
+++ app/src/main/java/com/allever/compose/green/vpn/LoadingActivity.kt
@@ -18,37 +18,42 @@ class LoadingActivity: Activity() {
 
     private val DURATION = 20 * 1000L
 
-    private var firstPercent = 0.7f
+    private var firstPercent = 0.7
     private var firstDuration = 0.2 * DURATION
     private var firstStep = (firstDuration / (firstPercent * 100)).toInt().toLong()
-    private var firstStart = 0
 
-    private var secondPercent = 0.2f
+    private var secondPercent = 0.2
     private var secondDuration = 0.3 * DURATION
     private var secondStep = (secondDuration / (secondPercent * 100)).toInt().toLong()
-    private var secondStart = firstDuration
 
-    private var finalPercent = 0.1f
+    private var finalPercent = 0.1
     private var finalDuration = 0.5 * DURATION
     private var finalStep = (finalDuration / (finalPercent * 100)).toInt().toLong()
-    private var finalStart = firstDuration + secondDuration
 
     private var isLoading = false
     private var job: Job? = null
+
+    private var finishFlag = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityLoadingBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
 
         mBinding.apply {
+            root.setOnClickListener {
+                finishFlag = true
+            }
             var step = firstStep
+            finishFlag = false
             job = GlobalScope.launch (Dispatchers.Default){
                 isLoading = true
                 var progress = 0
                 var usedTime = 0F
                 while (job?.isActive == true && progress < 100) {
                     delay(step)
-                    step = if (progress > 100 - finalPercent * 100) {
+                    step = if (finishFlag) {
+                        10
+                    }else if (progress > 100 - finalPercent * 100) {
                         finalStep
                     } else if (progress > firstPercent * 100) {
                         secondStep
